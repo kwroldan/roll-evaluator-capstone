a3de48f61a28128fb79563b61bb717b0e6d7ccca
Kristofer <kroldan@ford.com>
2022-06-10 14:17:48 -0400

    Add routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FeaturedWeaponsComponent } from './featured-weapons/featured-weapons.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'home',
+    component: LandingPageComponent
+  },{
+    path: '',
+    redirectTo: '/home',
+    pathMatch: 'full',
+  },{
+    path: 'featured-weapons',
+    component: FeaturedWeaponsComponent
+  // },{
+  //   path: 'roll-evaluator',
+  //   // component:
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
